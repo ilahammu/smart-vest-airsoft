3449d8fb5d1d042d425d88905753c7c34390a681
Ilham Muhijri <ilhammuhijri@gmail.com>
2025-04-20 00:32:19 +0700

    Membuat infomarsi (25%)

--- lib/app/modules/Drawer/Views/Information_view.dart
+++ lib/app/modules/Drawer/Views/Information_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:animate_do/animate_do.dart';
+
 import 'package:vest_keren/app/modules/Drawer/AppBar_Shared.dart';
 
 import '../Controllers/Information_controller.dart';
@@ -18,7 +21,7 @@ class InformationView extends GetView<InformationController> {
               decoration: const BoxDecoration(
                 gradient: LinearGradient(
                   begin: Alignment.bottomRight,
-                  end: Alignment.topRight,
+                  end: Alignment.centerRight,
                   colors: [
                     Color.fromARGB(255, 3, 0, 23),
                     Color.fromARGB(255, 21, 20, 33),
@@ -26,14 +29,70 @@ class InformationView extends GetView<InformationController> {
                   ],
                 ),
               ),
-              width: double.infinity,
-              height: 500,
-              child: Text(
-                "Information",
-                style: TextStyle(
-                  fontSize: 32,
-                  color: Colors.white,
-                ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  // Teks di kiri
+                  Expanded(
+                    flex: 2,
+                    child: SlideInLeft(
+                      key: UniqueKey(),
+                      duration: const Duration(milliseconds: 1000),
+                      delay: const Duration(milliseconds: 300),
+                      child: Container(
+                        padding: const EdgeInsets.all(40),
+                        child: Text(
+                          "SmartVest adalah proyek inovatif yang dirancang untuk meningkatkan pengalaman dan keakuratan dalam simulasi latihan tempur, khususnya permainan airsoft gun. Sistem ini mengintegrasikan teknologi ESP32, sensor piezoelectric, dan protokol komunikasi ESP-NOW untuk menciptakan rompi pintar yang mampu mendeteksi dan melaporkan titik benturan peluru secara real-time.",
+                          style: GoogleFonts.roboto(
+                            fontSize: 26,
+                            color: Colors.white,
+                          ),
+                          textAlign: TextAlign.justify,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 20),
+                  // Gambar di kanan
+                  Expanded(
+                    flex: 1,
+                    child: SlideInRight(
+                      key: UniqueKey(),
+                      duration: const Duration(milliseconds: 2000),
+                      delay: const Duration(milliseconds: 300),
+                      child: Column(
+                        children: [
+                          // Gambar 1
+                          Container(
+                            height: 200,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(30),
+                              image: const DecorationImage(
+                                image: AssetImage("assets/images/militer.jpg"),
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                          ),
+
+                          const SizedBox(height: 20),
+
+                          // Gambar 2
+                          Container(
+                            height: 200,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(30),
+                              image: const DecorationImage(
+                                image:
+                                    AssetImage("assets/images/militer2.jpeg"),
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
             Container(
